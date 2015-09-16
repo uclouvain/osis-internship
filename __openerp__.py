c98dd30957d0cb290f2cf3d2a99fcbc97a8320c2
Hildeberto Mendonca <hildeberto.mendonca@uclouvain.be>
2015-09-16 17:06:05 +0200

    changed the dependencies to cover the rename of the module osis to osis-core.

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','osis'],
+    'depends': ['base','osis-core'],
 
     # always loaded
     'data': [
