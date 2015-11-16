f2ee466edbf1f1582d487151b2fe659ff6ee7384
Leila Verpoorten <leila.verpoorten@uclouvain.be>
2015-11-16 12:44:12 +0100

    corrections person

--- models/person.py
+++ models/person.py
@@ -1,49 +1,93 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
 
 
 class Person(models.Model):
     _name = 'osis.person'
     _inherit = 'res.partner'
 
-    last_name = fields.Char('Last name')
-    first_name = fields.Char('First name')
+    last_name = fields.Char(_('Last name'))
+    first_name = fields.Char(_('First name'))
     middle_name = fields.Char('Middle name')
     global_id = fields.Char('Global ID')
     gender = fields.Selection([('F','Female'),('M','Male'),('U','Unknown')], default= 'U')
     national_number = fields.Char('National number')
 
-    # tutor_id = fields.Many2one('osis.tutor', string="Tutor")
+    def build_fullname(self, values):
 
-    # student = fields.Boolean("Student", compute = "_get_is_student")
-    #
-    # @api.one
-    # def _get_is_student(self):
-    #     nbr = self.env['osis.student'].sudo().search_count([('person_id','=',self.id)])
-    #     if nbr > 0:
-    #         self.student = True
-    #     else:
-    #         self.student = False
+        nom = ''
+        if values.get('last_name') is None:
+            if self.last_name:
+                nom = u"%s" % self.last_name
+        else:
+            nom = u"%s" % values.get('last_name')
+        prenom = ''
+        if values.get('last_name') is None:
+            if self.first_name:
+                prenom = u"%s" % self.first_name
+        else:
+            prenom = u"%s" % values.get('first_name')
+        prenoms = ''
+        if values.get('middle_name') is None:
+            if self.middle_name:
+                prenoms = u"%s" % self.middle_name
+        else:
+            prenoms = u"%s" % values.get('middle_name')
 
-    @api.multi
-    def write(self, vals):
-        name = str(vals.get('last_name')) + ' ' +str(vals.get('first_name'))
-        nom=''
-        if vals.get('last_name') is None:
-            nom = u"%s" % self.last_name
-        else:
-
-            nom = u"%s" % vals.get('last_name')
-        if vals.get('last_name') is None:
-            prenom = u"%s" % self.first_name
-        else:
-            prenom = u"%s" % vals.get('first_name')
-        if vals.get('middle_name') is None:
-            prenoms = u"%s" % self.middle_name
-        else:
-            prenoms = u"%s" % vals.get('middle_name')
         name = u"%s %s %s" % (nom,prenom,prenoms)
-        vals['name']=name
-        # self['name'] = self['last_name'] + self['first_name']
-        return super(Person, self).write(vals)
+        return name
+
+
+    def _build_fullname2(self):
+
+        nom = ''
+
+        if self.last_name:
+            nom = u"%s" % self.last_name
+
+        prenom = ''
+
+        if self.first_name:
+            prenom = u"%s" % self.first_name
+
+        prenoms = ''
+
+        if self.middle_name:
+            prenoms = u"%s" % self.middle_name
+
+
+        name = u"%s %s %s" % (nom,prenom,prenoms)
+        return name
+
+
+    # @api.model
+    # def create(self,values):
+    #     values['name']=build_fullname(self,values)
+    #     return super(Rental,self).create(values)
+    #
+    # @api.multi
+    # def write(self, values):
+    #     values['name']=build_fullname(self,values)
+    #     return super(Person, self).write(values)
+
+    @api.onchange('last_name','first_name','middle_name')
+    def _build_name(self):
+        nom = ''
+
+        if self.last_name:
+            nom = u"%s" % self.last_name
+
+        prenom = ''
+
+        if self.first_name:
+            prenom = u"%s" % self.first_name
+
+        prenoms = ''
+
+        if self.middle_name:
+            prenoms = u"%s" % self.middle_name
+
+
+        name = u"%s %s %s" % (nom,prenom,prenoms)
+        self.name = name
