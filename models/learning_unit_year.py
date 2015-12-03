c0ab5ab040aa5982161d519671308910a22c0e2d
Mona Strenaer <mona.strenaer@uclouvain.be>
2015-12-03 11:18:07 +0100

    données du nouveau modèle dans demo.xml et quelques corrections

--- models/learning_unit_year.py
+++ models/learning_unit_year.py
@@ -36,8 +36,6 @@ class Learning_unit_year(models.Model):
 
     academic_year_id = fields.Many2one('osis.academic_year', string='Academic year')
     learning_unit_id = fields.Many2one('osis.learning_unit', string='Learning unit')
-    attribution_entity_id = fields.Many2one('osis.structure', string='Attribution entity')
-    specifications_responsible_id = fields.Many2one('osis.structure', string='Specifications responsible entity')
 
     credits = fields.Float("Credits", default=0)
 
