76ca33916cd09a5190dbdba27241f46113b2c418
Hildeberto Mendonca <me@hildeberto.com>
2015-12-10 23:55:33 +0100

    Basic user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.conf.urls.i18n import i18n_patterns
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^login/$', views.user_login, name='login'),
     url(r'^studies/assessements/$', views.assessements)]
