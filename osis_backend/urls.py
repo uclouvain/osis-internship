fbd222ca28a1f7fc814a3127dad305a3599655be
Hildeberto Mendonca <me@hildeberto.com>
2016-01-09 15:03:52 +0100

    Changed the title of the admin site

--- osis_backend/urls.py
+++ osis_backend/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
 
 handler404 = 'core.views.page_not_found'
 handler403 = 'core.views.access_denied'
+
+admin.site.site_header = 'OSIS'
+admin.site.site_title  = 'OSIS'
+admin.site.index_title = 'Louvain'
