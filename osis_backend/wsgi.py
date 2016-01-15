1f4aafad867bb44ae6e273bd99784862f5283089
Gaëtan La Marca <lamarca.gaetan@gmail.com>
2016-01-15 15:51:00 +0100

    Update wsgi.py

--- osis_backend/wsgi.py
+++ osis_backend/wsgi.py
@@ -30,6 +30,5 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../osis_backend')
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "osis_backend.settings")
 application = get_wsgi_application()
