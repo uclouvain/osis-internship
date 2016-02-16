a6f50015d378f71dd25f92509a0dc0aca62c122f
Leila Verpoorten <leila.verpoorten@uclouvain.be>
2016-02-16 12:43:24 +0100

    corrections sonar

--- core/views_osis/learning_unit.py
+++ core/views_osis/learning_unit.py
@@ -38,7 +38,7 @@ def learning_units(request):
     academic_years = AcademicYear.find_academic_years()
     academic_year_calendar = AcademicCalendar.current_academic_year()
 
-    if not(academic_year_calendar is None):
+    if not academic_year_calendar is None:
         academic_year = academic_year_calendar.id
     return render(request, "learning_units.html", {
                                            'academic_year':  academic_year,
@@ -59,7 +59,7 @@ def learning_units_search(request):
 
     if academic_year is None:
         academic_year_calendar = AcademicCalendar.current_academic_year()
-        if not(academic_year_calendar is None):
+        if not academic_year_calendar is None:
             academic_year = academic_year_calendar.id
 
     learning_units = LearningUnitYear.find_learning_unit_years_by_academic_year(academic_year)
