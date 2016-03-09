e5a274072e949337e179de3415661f7cb3723649
Maxime Lambot <maxime_lambot@hotmail.com>
2016-03-09 15:18:39 +0100

    Adapt the code to the branch dev, separate the views into one file pe…

---
+++ internship/_views.py
@@ -0,0 +1,81 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from internship.models import InternshipOffer, InternshipMaster
+from base import models as mdl
+
+
+@login_required
+def internships_home(request):
+    return render(request, "internships_home.html", {'section': 'internship'})
+
+
+@login_required
+def internships_places(request):
+    organizations = mdl.organization.find_all_order_by_reference()
+
+    if organizations:
+        for organization in organizations:
+            organization.address = ""
+            address = mdl.organization_address.find_by_organization(organization.id)
+            if address:
+                organization.address = address
+
+    return render(request, "places.html", {'section': 'internship', 'all_organizations' : organizations})
+
+
+@login_required
+def internships_students(request):
+    students = None
+
+    if students:
+        for student in students:
+            student.address = ""
+            address = mdl.person_address.find_by_person(student.person)
+            if address:
+                student.address = address
+
+    return render(request, "students.html", {'section': 'internship', 'all_students': students})
+
+
+@login_required
+def internships(request):
+    query = InternshipOffer.find_internships()
+
+    return render(request, "internships.html", {'section': 'internship', 'all_internships': query})
+
+
+@login_required
+def internships_periods(request):
+    return render(request, "periods.html", {'section': 'internship'})
+
+
+@login_required
+def interships_masters(request):
+    query = InternshipMaster.find_masters()
+
+    return render(request, "interships_masters.html", {'section': 'internship', 'all_masters': query})
