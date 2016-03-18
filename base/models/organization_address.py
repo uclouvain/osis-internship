e6594989c845e0c7a61ca6d930aba72e63accbe6
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-03-18 14:57:27 +0100

    issue#281 usage of foreign key Strings instead of Classes

--- base/models/organization_address.py
+++ base/models/organization_address.py
@@ -34,7 +34,7 @@ class OrganizationAddressAdmin(admin.ModelAdmin):
 
 
 class OrganizationAddress(models.Model):
-    organization = models.ForeignKey(organization.Organization)
+    organization = models.ForeignKey('Organization')
     label        = models.CharField(max_length=20)
     location     = models.CharField(max_length=255)
     postal_code  = models.CharField(max_length=20)
