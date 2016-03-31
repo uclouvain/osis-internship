6e89fbc554d572156ded4b31ee4bed32f0e97b92
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-03-31 11:50:17 +0200

    #302 created 'admission' app with form, question & option models (with migration file)

---
+++ admission/models/option.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+# OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.db import models
+from django.contrib import admin
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ('label', 'description')
+    fieldsets = ((None, {'fields': ('label', 'value', 'order', 'description', 'question')}),)
+
+class Option(models.Model):
+    changed = models.DateTimeField(null=True)
+    label = models.CharField(max_length=255)
+    value = models.TextField()
+    order = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    question = models.ForeignKey('Question')
+
+    def __str__(self):
+        return u"%s" % (self.label)
