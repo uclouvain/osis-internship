86665d2abc2a3c6677d18fe6ccbc90c870f1c392
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-04-05 16:39:49 +0200

    #314 usage of exchange param to connect to the queue (without routing key)

--- backoffice/queue.py
+++ backoffice/queue.py
@@ -5,8 +5,7 @@ def send_message(queue_name, message):
     credentials = pika.PlainCredentials(QUEUE_USER, QUEUE_PASSWORD)
     connection = pika.BlockingConnection(pika.ConnectionParameters(QUEUE_URL, QUEUE_PORT, QUEUE_CONTEXT_ROOT, credentials))
     channel = connection.channel()
-    channel.queue_declare(queue=queue_name)
-    channel.basic_publish(exchange='',
-                          routing_key=queue_name,
+    channel.basic_publish(exchange=queue_name,
+                          routing_key='',
                           body=message)
     connection.close()
