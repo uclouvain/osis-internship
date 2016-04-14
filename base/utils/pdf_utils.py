d20cdd9b07f4531f599154b9848ae9fdca2912dd
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-04-14 14:17:24 +0200

    #367 updated translation files

--- base/utils/pdf_utils.py
+++ base/utils/pdf_utils.py
@@ -199,13 +199,14 @@ def list_notes_building(session_exam, learning_unit_year_id, academic_year, list
             justification = ""
             if rec_exam_enrollment.justification_final:
                 justification = dict(mdl.exam_enrollment.JUSTIFICATION_TYPES)[rec_exam_enrollment.justification_final]
+            end_date = session_exam.offer_year_calendar.end_date # end_date ne devrait jamais etre None !
             data.append([student.registration_id,
                          person.last_name,
                          person.first_name,
                          score,
                          justification,
-                         session_exam.offer_year_calendar.end_date.strftime('%d/%m/%Y')])
-        cpt = cpt + 1
+                         end_date.strftime('%d/%m/%Y')])
+        cpt += 1
 
     if not old_pgm is None:
         main_data(academic_year, session_exam, styles, current_learning_unit_year, old_pgm, content)
@@ -336,7 +337,7 @@ def end_page_infos_building(content):
     p = ParagraphStyle('info')
     p.fontSize = 10
     p.alignment = TA_LEFT
-    content.append(Paragraph("Please return this document to the administrative office of the program administrator"
+    content.append(Paragraph(_("return_doc_to_administrator")
                              , p))
     content.append(Paragraph('''
                             <para spaceb=10>
