9d0912902c2c2ce16aaa3350a34b566854525328
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2016-04-18 12:13:40 +0200

    removed _start_date and _end_date

--- base/models/offer_year_calendar.py
+++ base/models/offer_year_calendar.py
@@ -42,22 +42,9 @@ class OfferYearCalendar(models.Model):
     changed           = models.DateTimeField(null=True)
     academic_calendar = models.ForeignKey('AcademicCalendar')
     offer_year        = models.ForeignKey('OfferYear')
-    _start_date       = models.DateField(db_column="start_date", auto_now=False, blank=True, null=True, auto_now_add=False)
-    _end_date         = models.DateField(db_column="end_date", auto_now=False, blank=True, null=True, auto_now_add=False)
+    start_date       = models.DateField(default=timezone.now)
+    end_date         = models.DateField(default=timezone.now)
     customized        = models.BooleanField(default=False)
-
-    @property
-    def start_date(self):
-        if self._start_date :
-            return self._start_date
-        return self.academic_calendar.start_date
-
-    @property
-    def end_date(self):
-        if self._end_date :
-            return self._end_date
-        return self.academic_calendar.end_date
-
 
     def __str__(self):
         return u"%s - %s" % (self.academic_calendar, self.offer_year)
