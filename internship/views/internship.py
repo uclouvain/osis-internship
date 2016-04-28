7379ec071ecade9a81d20c2670be41079a2a1466
Maxime Lambot <maxime_lambot@hotmail.com>
2016-04-28 10:46:04 +0200

    Add a float casting (data came in string)

--- internship/views/internship.py
+++ internship/views/internship.py
@@ -64,8 +64,10 @@ def strip_accents(s):
                   if unicodedata.category(c) != 'Mn')
 
 def calc_dist(lat_a, long_a, lat_b, long_b):
-    lat_a = radians(lat_a)
-    lat_b = radians(lat_b)
+    lat_a = radians(float(lat_a))
+    lat_b = radians(float(lat_b))
+    long_a = float(long_a)
+    long_b = float(long_b)
     long_diff = radians(long_a - long_b)
     distance = (sin(lat_a) * sin(lat_b) +
                 cos(lat_a) * cos(lat_b) * cos(long_diff))
