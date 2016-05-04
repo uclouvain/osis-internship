7e90ac454b7c571c8bfa190be11b53728e25576b
Hildeberto Mendonca <me@hildeberto.com>
2016-05-04 14:29:25 +0200

    Fixed migration

--- base/migrations/0040_scoresencoding.py
+++ base/migrations/0040_scoresencoding.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0038_auto_20160502_1559'),
+        ('base', '0039_auto_20160502_0055'),
     ]
 
     operations = [
