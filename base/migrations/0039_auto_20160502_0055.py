1fba32c228d8f08bd4763cdb4e36c47349130850
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2016-05-04 13:42:48 +0200

    #561 fix migration issue

--- base/migrations/0039_auto_20160502_0055.py
+++ base/migrations/0039_auto_20160502_0055.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0037_auto_20160427_1211'),
+        ('base', '0038_auto_20160502_1559.py'),
     ]
 
     operations = [
