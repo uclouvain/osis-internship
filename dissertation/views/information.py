654c8b9f918e947ae2d31a3f944282fa431332b2
Christophe Fivet <christophe.fivet@uclouvain.be>
2016-05-04 11:55:22 +0200

    Correction des variable p en adviser pour Information Edit

--- dissertation/views/information.py
+++ dissertation/views/information.py
@@ -41,9 +41,8 @@ def is_manager(user):
 def informations(request):
     person = mdl.person.find_by_user(request.user)
     try:
-        p = Adviser(person=person, email_accept=False, phone_accept=False, office_accept=False)
-        p.save()
-        adviser = Adviser.find_by_person(person)
+        adviser = Adviser(person=person, email_accept=False, phone_accept=False, office_accept=False)
+        adviser.save()
     except :
         adviser = Adviser.find_by_person(person)
     return render(request, "informations.html", {'adviser': adviser})
