1be0d3374ad9b2fd3d0e0be1c5c71c727acadcbc
Gaetan La Marca <gaetan.lamarca@uclouvain.be>
2016-05-11 12:12:25 +0200

    #593 methide dans osis.js + modif nom methode

--- base/static/js/osis.js
+++ base/static/js/osis.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
 $("#bt_remove").click(function(event) {
     return confirm("Oui?");
 });
+
+function invalidScoreMsg(input,decimal_allowed,message_decimal,message_max_score){
+    if (input.value > 20){
+        input.setCustomValidity(message_max_score)
+    }
+    else if (input.value != Math.floor(input.value) && !decimal_allowed){
+        input.setCustomValidity(message_decimal)
+    }
+    else{
+         input.setCustomValidity('');
+    }
+}
