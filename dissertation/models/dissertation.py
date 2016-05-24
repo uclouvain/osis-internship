884f200726e8bc7a480ec00cdeef7f2456551314
Laurent Dermine <Laurent.Dermine@uclouvain.be>
2016-05-24 16:20:09 +0200

    ajout de fontionnalités : requetes et d affichage des charges des enseignants

--- dissertation/models/dissertation.py
+++ dissertation/models/dissertation.py
@@ -50,8 +50,7 @@ class Dissertation(models.Model):
         ('TO_RECEIVE', _('To be received')),
         ('TO_DEFEND', _('To be defended')),
         ('DEFENDED', _('Defended')),
-        ('ENDED_WIN', _('Ended Win')),
-        ('ENDED_LOS', _('Ended Los')),
+        ('ENDED', _('Ended')),
     )
 
     title = models.CharField(max_length=200)
--- dissertation/views/information.py
+++ dissertation/views/information.py
@@ -31,6 +31,7 @@ from base import models as mdl
 from dissertation.forms import AdviserForm, ManagerAdviserForm
 from django.contrib.auth.decorators import user_passes_test
 from django.db import IntegrityError
+from django.db.models import Q
 
 
 def is_manager(user):
@@ -48,9 +49,31 @@ def informations(request):
     except IntegrityError:
         adviser = Adviser.find_by_person(person)
 
-    count_advisers = DissertationRole.objects.filter(adviser=adviser).count()
-    return render(request, "informations.html", {'adviser': adviser, 'count_advisers': count_advisers})
+        queryset = DissertationRole.objects.all()
 
+        count_advisers = queryset.filter(Q(adviser=adviser)& Q(dissertation__active=True)).count()
+
+        count_advisers_pro = queryset.filter(Q(adviser=adviser) &
+        Q(status='PROMOTEUR')&
+        Q(dissertation__active=True)).exclude(Q(dissertation__status='DRAFT')|
+        Q(dissertation__status='ENDED')|Q(dissertation__status='DEFENDED')).count()
+
+        count_advisers_pro_request = queryset.filter(
+        Q(adviser=adviser) & Q(status='PROMOTEUR')&
+        Q(dissertation__status='DIR_SUBMIT')& Q(dissertation__active=True)).count()
+
+        count_advisers_copro= queryset.filter(Q(adviser=adviser) &
+        Q(status='CO_PROMOTEUR')& Q(dissertation__active=True)).exclude(
+        Q(dissertation__status='DRAFT')|Q(dissertation__status='ENDED')|
+        Q(dissertation__status='DEFENDED')).count()
+
+        count_advisers_reader= queryset.filter(Q(adviser=adviser) &
+        Q(status='READER')& Q(dissertation__active=True)).exclude(Q(dissertation__status='DRAFT')|
+        Q(dissertation__status='ENDED')|Q(dissertation__status='DEFENDED')).count()
+
+    return render(request, "informations.html", {'adviser': adviser, 'count_advisers':count_advisers,'count_advisers_CoPro':count_advisers_copro,
+    'count_advisers_pro':count_advisers_pro, 'count_advisers_reader':count_advisers_reader,
+    'count_advisers_pro_request':count_advisers_pro_request })
 
 @login_required
 def informations_edit(request):
@@ -79,8 +102,37 @@ def manager_informations(request):
 @user_passes_test(is_manager)
 def manager_informations_detail(request, pk):
     adviser = get_object_or_404(Adviser, pk=pk)
-    count_advisers = DissertationRole.objects.filter(adviser=adviser).count()
-    return render(request, 'manager_informations_detail.html', {'adviser': adviser, 'count_advisers':count_advisers})
+
+
+    queryset = DissertationRole.objects.all()
+
+    count_advisers = queryset.filter(Q(adviser=adviser)& Q(dissertation__active=True)).count()
+
+    count_advisers_pro = queryset.filter(
+    Q(adviser=adviser) & Q(status='PROMOTEUR')&
+    Q(dissertation__active=True)).exclude(Q(dissertation__status='DRAFT')|
+    Q(dissertation__status='ENDED')|Q(dissertation__status='DEFENDED')).count()
+
+    count_advisers_pro_request = queryset.filter(
+    Q(adviser=adviser) & Q(status='PROMOTEUR')&
+    Q(dissertation__status='DIR_SUBMIT')& Q(dissertation__active=True)).count()
+
+    count_advisers_copro= queryset.filter(
+    Q(adviser=adviser) & Q(status='CO_PROMOTEUR')&
+    Q(dissertation__active=True)).exclude(
+    Q(dissertation__status='DRAFT')|Q(dissertation__status='ENDED')|
+    Q(dissertation__status='DEFENDED')).count()
+
+    count_advisers_reader= queryset.filter(Q(adviser=adviser) &
+    Q(status='READER')& Q(dissertation__active=True)).exclude(
+    Q(dissertation__status='DRAFT')|
+    Q(dissertation__status='ENDED')|Q(dissertation__status='DEFENDED')).count()
+
+
+    return render(request, 'manager_informations_detail.html',
+    {'adviser': adviser,'count_advisers':count_advisers,'count_advisers_CoPro':count_advisers_copro,
+    'count_advisers_pro':count_advisers_pro, 'count_advisers_reader':count_advisers_reader,
+    'count_advisers_pro_request':count_advisers_pro_request })
 
 
 @login_required
@@ -96,8 +148,8 @@ def manager_informations_edit(request, pk):
             return redirect('manager_informations')
     else:
         form = AdviserForm(instance=adviser)
-    count_advisers = DissertationRole.objects.filter(adviser=adviser).count()
-    return render(request, "manager_informations_edit.html", {'adviser': adviser, 'form': form, 'count_advisers':count_advisers})
+
+    return render(request, "manager_informations_edit.html", {'adviser': adviser, 'form': form})
 
 
 @login_required
