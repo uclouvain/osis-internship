d5b729225e4bb84aeb8c34aed0299e10dd7909ed
Maxime Lambot <maxime_lambot@hotmail.com>
2016-05-27 09:36:18 +0200

    Delete uncessary columns

--- internship/models.py
+++ internship/models.py
@@ -151,8 +151,6 @@ class InternshipChoice(models.Model):
 
 
 class Organization(models.Model):
-    external_id = models.CharField(max_length=100, blank=True, null=True)
-    changed = models.DateTimeField(null=True)
     name = models.CharField(max_length=255)
     acronym = models.CharField(max_length=15)
     website = models.URLField(max_length=255, blank=True, null=True)
