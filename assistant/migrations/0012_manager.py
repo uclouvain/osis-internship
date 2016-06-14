ad91b2497bb3bd5147b6098603b3238cedf2bb32
Laurent Buset <laurent.buset@uclouvain.be>
2016-06-14 15:05:23 +0200

    remove unwanted dependency

--- assistant/migrations/0012_manager.py
+++ assistant/migrations/0012_manager.py
@@ -9,7 +9,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0054_auto_20160614_1030'),
         ('assistant', '0011_auto_20160610_1148'),
     ]
 
