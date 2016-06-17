deb549f06dcec433a8b949c8f9d470122e6fdeb2
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2016-06-17 10:11:31 +0200

    Corrections noms variables dates offer_proposition

--- dissertation/models/offer_proposition.py
+++ dissertation/models/offer_proposition.py
@@ -57,26 +57,24 @@ class OfferProposition(models.Model):
     # Is there a validation commission ?
     validation_commission_exists = models.BooleanField(default=False)
 
-    ################
-    # New feelds
-    ################
+    #################
+    # DATE PARAMETERS
+    #################
     #   Start of visibility of proposition_dissertation
-
-    month_start_visibility_proposition=models.IntegerField(default=9)
-    day_of_month_start_visibility_proposition=models.IntegerField(default=1)
+    month_start_visibility_proposition = models.IntegerField(default=9)
+    day_of_month_start_visibility_proposition = models.IntegerField(default=1)
 
     #   End of visibility of proposition_dissertation
-    month_start_visibility_proposition=models.IntegerField(default=8)
-    day_of_month_start_visibility_proposition=models.IntegerField(default=31)
+    month_end_visibility_proposition = models.IntegerField(default=8)
+    day_of_month_end_visibility_proposition = models.IntegerField(default=31)
 
-    #   Start of oppening dissertation
-
-    month_start_visibility_dissertation=models.IntegerField(default=9)
-    day_of_month_start_visibility_dissertation=models.IntegerField(default=1)
+    #   Start of opening dissertation
+    month_start_visibility_dissertation = models.IntegerField(default=9)
+    day_of_month_start_visibility_dissertation = models.IntegerField(default=1)
 
     #   End of oppening dissertation
-    month_start_visibility_dissertation=models.IntegerField(default=8)
-    day_of_month_start_visibility_dissertation=models.IntegerField(default=31)
+    month_end_visibility_dissertation = models.IntegerField(default=8)
+    day_of_month_end_visibility_dissertation = models.IntegerField(default=31)
 
     def __str__(self):
         return self.acronym
