1cea44ab6c9395c3db4cd0995efb10db9b6dea5a
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2016-07-15 11:55:36 +0200

    issue#1110 ajout champ date défense prévue dans dissertation (+ views + forms)

--- dissertation/forms.py
+++ dissertation/forms.py
@@ -67,13 +67,15 @@ class ManagerAdviserForm(ModelForm):
 class ManagerDissertationForm(ModelForm):
     class Meta:
         model = Dissertation
-        fields = ('title', 'author', 'offer_year_start', 'proposition_dissertation', 'description')
+        fields = ('title', 'author', 'offer_year_start', 'proposition_dissertation', 'description', 'defend_periode',
+                  'defend_year')
 
 
 class ManagerDissertationEditForm(ModelForm):
     class Meta:
         model = Dissertation
-        fields = ('title', 'author', 'offer_year_start', 'proposition_dissertation', 'description', 'defend_periode')
+        fields = ('title', 'author', 'offer_year_start', 'proposition_dissertation', 'description', 'defend_periode',
+                  'defend_year')
 
 
 class ManagerDissertationRoleForm(ModelForm):
--- dissertation/models/dissertation.py
+++ dissertation/models/dissertation.py
@@ -29,6 +29,7 @@ from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 from base.models import offer_year, student
 from . import proposition_dissertation
+from datetime import datetime
 
 
 class DissertationAdmin(admin.ModelAdmin):
@@ -64,10 +65,13 @@ class Dissertation(models.Model):
         ('SEPTEMBER', _('september')),
     )
 
+    DEFEND_YEAR_CHOICES = tuple((str(n), str(n)) for n in range(2016, datetime.now().year + 5))
+
     title = models.CharField(max_length=200)
     author = models.ForeignKey(student.Student)
     status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='DRAFT')
     defend_periode = models.CharField(max_length=12, choices=DEFEND_PERIODE_CHOICES, default='UNDEFINED')
+    defend_year = models.CharField(max_length=4, choices=DEFEND_YEAR_CHOICES, default='2016')
     offer_year_start = models.ForeignKey(offer_year.OfferYear)
     proposition_dissertation = models.ForeignKey(proposition_dissertation.PropositionDissertation)
     description = models.TextField(blank=True, null=True)
