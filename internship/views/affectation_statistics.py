47a45a7ac98314331db13430a5a500dc1569970e
Maxime Lambot <maxime_lambot@hotmail.com>
2016-09-08 11:04:50 +0200

    Start the display of the affactations

--- internship/views/affectation_statistics.py
+++ internship/views/affectation_statistics.py
@@ -867,12 +867,16 @@ def internship_affectation_statistics(request):
 def internship_affectation_sumup(request):
     all_speciality = InternshipSpeciality.search(mandatory=True).order_by("acronym", "name")
     periods = Period.search().order_by("date_start")
-    places = Organization.search()
-    places = sort_organizations(places)
+    organizations = Organization.search()
+    organizations = sort_organizations(organizations)
+    affectations = InternshipStudentAffectationStat.search().order_by("student__person__last_name", "student__person__first_name", "period__date_start")
+    for a in affectations:
+        print(a.period.name)
 
     return render(request, "internship_affectation_sumup.html",
                                                 {'section': 'internship',
                                                  'specialities':        all_speciality,
                                                  'periods':             periods,
-                                                 'places':              places,
+                                                 'organizations':       organizations,
+                                                 'affectations':        affectations,
                                                 })
