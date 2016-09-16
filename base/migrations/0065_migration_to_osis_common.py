0f88724a235c7e40865c30964f9e21404f7d86ca
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2016-09-16 13:26:23 +0200

    #1288 modif fichier migration pour suivre etat dev

--- base/migrations/0065_migration_to_osis_common.py
+++ base/migrations/0065_migration_to_osis_common.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0061_auto_20160902_1639'),
+        ('base', '0064_remove_uuid_null'),
     ]
 
     operations = [
