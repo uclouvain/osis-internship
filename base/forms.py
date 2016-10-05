e873c8f8708ccee77d68d05b5f9e5e4428fea290
Hildeberto Mendonca <me@hildeberto.com>
2016-10-05 20:59:53 +0200

    Solved conflit with dev

--- base/forms.py
+++ base/forms.py
@@ -81,22 +81,6 @@ class OfferYearCalendarForm(ModelForm):
         fields = ['offer_year', 'start_date', 'end_date', 'customized']
 
 
-class MessageTemplateForm(ModelForm):
-    template = forms.CharField(widget=CKEditorWidget)
-
-    class Meta:
-        model = mdl.message_template.MessageTemplate
-        fields = ['reference', 'subject', 'template', 'format', 'language']
-
-
-class MessageHistorySearchForm(forms.Form):
-    subject = forms.CharField(max_length=50, required=False)
-    reference = forms.CharField(max_length=50, required=False)
-    recipient = forms.CharField(max_length=25, required=False)
-    origin = forms.CharField(max_length=25, required=False)
-    not_sent = forms.BooleanField(initial=False, required=False)
-
-
 class OfferFormForm(ModelForm):
     class Meta:
         model = admission.form.Form
