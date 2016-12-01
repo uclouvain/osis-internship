9438e82587245c35d0ba84ed07616840830dbd8b
edd19 <eddyndizera@gmail.com>
2016-12-01 17:12:56 +0100

    correctio bug

--- base/views/score_encoding.py
+++ base/views/score_encoding.py
@@ -208,7 +208,7 @@ def update_exam_enrollments(request, exam_enrollments, decimal_scores_authorized
 def update_exam_enrollment(request, is_pgm, decimal_scores_authorized, enrollment):
     score = request.POST.get('score_' + str(enrollment.id), None)
     justification = request.POST.get('justification_' + str(enrollment.id), None)
-    score_changed = request.POST.get('score_changed_' + str(enrollment.id), 'false')
+    score_changed = request.POST.get('score_changed_' + str(enrollment.id), 'not set')
     # modification is possible for program managers OR score has changed but nothing is final
     if is_pgm or is_legible_for_modifying_exam_enrollment(score_changed, enrollment):
         new_score, new_justification = _truncate_decimals(score, justification, decimal_scores_authorized)
@@ -239,6 +239,8 @@ def set_score_and_justification_for_exam_enrollment(is_pgm, enrollment, new_just
 
 
 def is_legible_for_modifying_exam_enrollment(score_changed, exam_enrollment):
+    if score_changed == 'not set':
+        return not exam_enrollment.score_final and not exam_enrollment.justification_final
     return score_changed == "true" and not exam_enrollment.score_final and not exam_enrollment.justification_final
 
 
