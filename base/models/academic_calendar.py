3c4608bed9d8de84f0580e5cc7c1cd84b02cfa81
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-01-20 08:19:01 +0100

    #1678

--- base/models/academic_calendar.py
+++ base/models/academic_calendar.py
@@ -43,7 +43,7 @@ class AcademicCalendar(SerializableModel):
     highlight_title = models.CharField(max_length=255, blank=True, null=True)
     highlight_description = models.CharField(max_length=255, blank=True, null=True)
     highlight_shortcut = models.CharField(max_length=255, blank=True, null=True)
-    reference = models.CharField(max_length=25, blank=True, null=True)
+    reference = models.CharField(max_length=50, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if FUNCTIONS not in kwargs.keys():
