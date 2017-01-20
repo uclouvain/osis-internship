a4dc51844c7e804c43551792d57853f6f5d59678
DefatDavid <[defat.david]@gmail.com>
2017-01-20 16:20:38 +0100

    makemigrations

--- base/models/__init__.py
+++ base/models/__init__.py
@@ -39,6 +39,7 @@ from base.models import learning_container
 from base.models import learning_container_year
 from base.models import learning_unit
 from base.models import learning_unit_component
+from base.models import learning_unit_component_class
 from base.models import learning_unit_enrollment
 from base.models import learning_unit_year
 from base.models import native
