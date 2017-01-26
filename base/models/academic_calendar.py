e335c8099480d2076bb870fc84344831222f72f4
Gaëtan La Marca <lamarca.gaetan@gmail.com>
2017-01-27 00:45:04 +0100

    Revert "Revert "Release 0.16.0""

--- base/models/academic_calendar.py
+++ base/models/academic_calendar.py
@@ -27,11 +27,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils import timezone
 from base.models.exceptions import FunctionAgrumentMissingException, StartDateHigherThanEndDateException
+from osis_common.models.serializable_model import SerializableModel
 
 FUNCTIONS = 'functions'
 
 
-class AcademicCalendar(models.Model):
+class AcademicCalendar(SerializableModel):
     external_id = models.CharField(max_length=100, blank=True, null=True)
     changed = models.DateTimeField(null=True)
     academic_year = models.ForeignKey('AcademicYear')
@@ -42,6 +43,7 @@ class AcademicCalendar(models.Model):
     highlight_title = models.CharField(max_length=255, blank=True, null=True)
     highlight_description = models.CharField(max_length=255, blank=True, null=True)
     highlight_shortcut = models.CharField(max_length=255, blank=True, null=True)
+    reference = models.CharField(max_length=50, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if FUNCTIONS not in kwargs.keys():
