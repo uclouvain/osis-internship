d5f03508a7389edbb0fb849313e12d30d2437da0
Laurent Dermine <Laurent.Dermine@uclouvain.be>
2017-02-13 17:21:57 +0100

    ajout fonctionnalité la photos via view person.py

--- base/views/person.py
+++ base/views/person.py
@@ -1,33 +1,24 @@
-import base64
 import urllib
-
 from django.http import HttpResponse
-
 from base.models import person
 from backoffice.settings import PERSON_PHOTO_PATH
 
 
 def get_avatar(request, person_id):
-    a_person = person.find_by_id(person_id)
-    photo = get_photo(a_person)
-    return HttpResponse(photo, content_type="image/jpeg")
+    try:
+        a_person = person.find_by_id(person_id)
+    except IOError:
+        return None
 
-
-def get_photo(person):
-    # Return JPG in Base64 format
-    # return None if no valid data: global_id or no picture
-    # for template use <img src="data:image/jpeg;base64,{{person.get_photo}}" class="avatar img-responsive"/>
-    # timeout 1 sec with URLLIB request
-
-    if person.get_photo_path():
+    if a_person and PERSON_PHOTO_PATH != '':
         try:
-            photo = urllib.request.urlopen(person.get_photo_path(), None, 1.0)
-            photo_base64 = base64.b64encode(photo.read())
-            return photo_base64
+            photo = urllib.request.urlopen(get_photo_path(a_person), None,1)
         except IOError:
             return None
     else:
         return None
+    return HttpResponse(photo, content_type="image/jpeg")
+
 
 
 def get_photo_path(person):
