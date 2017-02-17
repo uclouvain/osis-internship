f284c99a172d85788e0735ab4f5e8216fd315af2
Hildeberto Mendonca <me@hildeberto.com>
2017-02-17 14:44:19 +0100

    PEP8 dans le fichier.

--- base/views/person.py
+++ base/views/person.py
@@ -2,8 +2,10 @@ import urllib
 from django.http import HttpResponse
 from base.models import person
 from backoffice.settings import PERSON_PHOTO_PATH
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def get_avatar(request, person_id):
     try:
         a_person = person.find_by_id(person_id)
@@ -12,13 +14,12 @@ def get_avatar(request, person_id):
 
     if a_person and PERSON_PHOTO_PATH != '':
         try:
-            photo = urllib.request.urlopen(get_photo_path(a_person), None,1)
+            photo = urllib.request.urlopen(get_photo_path(a_person), None, 1)
         except IOError:
             return None
     else:
         return None
     return HttpResponse(photo, content_type="image/jpeg")
-
 
 
 def get_photo_path(person):
