4479c459753dc0a45f6067ab93906917ad3eda69
Ndizera Eddy <eddyndizera@gmail.com>
2017-02-26 19:46:11 +0100

    read file

---
+++ internship/tests/utils/test_affect_student.py
@@ -0,0 +1,46 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.test import SimpleTestCase
+from internship.utils import affect_student
+
+
+class TestAffectStudent(SimpleTestCase):
+    def test_input_file(self):
+        actual_offers, actual_choices = affect_student.input_file("./internship/tests/utils/ressources/sample1.txt")
+        expected_offers = {(1, 1): [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2],
+                           (2, 1): [2, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 2],
+                           (2, 2): [3, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2],
+                           (3, 3): [4, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2],
+                           (4, 3): [5, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2]}
+        expected_choices = {(1, 1, 1): [[1, 1, 1, 1, 1, 1], [1, 2, 1, 1, 2, 1]],
+                            (1, 2, 3): [[1, 4, 3, 2, 1, 1]],
+                            (2, 1, 1): [[2, 2, 1, 1, 1, 0], [2, 1, 1, 1, 2, 0]],
+                            (3, 1, 2): [[3, 2, 2, 1, 1, 0]],
+                            (3, 2, 3): [[3, 3, 3, 2, 1, 0], [3, 4, 3, 2, 2, 0]],
+                            (4, 1, 1): [[4, 1, 1, 1, 1, 1]],
+                            (4, 2, 3): [[4, 4, 3, 2, 1, 1]]}
+        self.assertDictEqual(actual_offers, expected_offers)
+        self.assertDictEqual(actual_choices, expected_choices)
---
+++ internship/utils/affect_student.py
@@ -0,0 +1,56 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2016 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+
+def input_file(filename):
+    with open(filename) as file:
+        number_offers, number_choices = [int(x) for x in file.readline().split()]
+        file.readline()
+        offers = extract_offers(file, number_offers)
+        file.readline()
+        choices = extract_choices(file, number_choices)
+        return offers, choices
+
+
+def extract_offers(file, number_offers):
+    offers = dict()
+    for x in range(0, number_offers):
+        offer = [int(x) for x in file.readline().split()]
+        offers[(offer[1], offer[2])] = offer
+    return offers
+
+
+def extract_choices(file, number_choices):
+    choices = dict()
+    for x in range(0, number_choices):
+        choice = [int(x) for x in file.readline().split()]
+        key = (choice[0], choice[3], choice[2])
+        if key in choices:
+            choices[key].append(choice)
+        else:
+            choices[key] = [choice]
+    return choices
+
