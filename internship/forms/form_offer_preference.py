acb5db02c87b565170cbdc1559b499f73bbf96c5
Ndizera Eddy <eddyndizera@gmail.com>
2017-03-08 14:26:50 +0100

    added periods to offer preference formset

--- internship/forms/form_offer_preference.py
+++ internship/forms/form_offer_preference.py
@@ -36,8 +36,24 @@ class OfferPreferenceForm(forms.Form):
         ('3', _('third_choice')),
         ('4', _('fourth_choice'))
     )
+    PERIODS = (
+        ("P1", "P1"),
+        ("P2", "P2"),
+        ("P3", "P3"),
+        ("P4", "P4"),
+        ("P5", "P5"),
+        ("P6", "P6"),
+        ("P7", "P7"),
+        ("P8", "P8"),
+        ("P9", "P9"),
+        ("P10", "P10"),
+        ("P11", "P11"),
+        ("P12", "P12"),
+
+    )
     offer = forms.IntegerField()
     preference = forms.ChoiceField(choices=PREFERENCE_CHOICES, required=True)
+    periods = forms.MultipleChoiceField(choices=PERIODS, required=True)
     priority = forms.BooleanField(required=False)
 
 
