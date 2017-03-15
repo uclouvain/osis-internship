e18e2ec7d154c930cda173218f73227cf3830562
DefatDavid <[defat.david]@gmail.com>
2017-03-15 16:09:39 +0100

    Dont put .py at the end of the dependencies

--- base/migrations/0098_auto_20170306_0953.py
+++ base/migrations/0098_auto_20170306_0953.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0097_learningunitcomponent_coefficient_repetition.py'),
+        ('base', '0097_learningunitcomponent_coefficient_repetition'),
     ]
 
     operations = [
--- base/migrations/0099_auto_20170308_1328.py
+++ base/migrations/0099_auto_20170308_1328.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0098_auto_20170306_0953.py'),
+        ('base', '0098_auto_20170306_0953'),
     ]
 
     operations = [
