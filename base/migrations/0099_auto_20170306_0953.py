fd5c737e500b5ac2a6f109990087d7910b4b40d9
DefatDavid <[defat.david]@gmail.com>
2017-03-15 15:42:18 +0100

    #1760 Migration files corrected

--- base/migrations/0099_auto_20170306_0953.py
+++ base/migrations/0099_auto_20170306_0953.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0098_learningunitcomponent_coefficient_repetition'),
+        ('base', '0098_auto_20170306_0953.py'),
     ]
 
     operations = [
