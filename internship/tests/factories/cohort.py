5a7012f79c15f8ed1d5597b1fe9cb17bc4a99e9e
Stephane Wirtel <stephane@wirtel.be>
2017-03-24 10:59:11 +0100

    Change the label of the cohort by the CohortFactory

--- internship/tests/factories/cohort.py
+++ internship/tests/factories/cohort.py
@@ -6,6 +6,6 @@ class CohortFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'internship.Cohort'
 
-    name = factory.Sequence(lambda n: str(n))
+    name = factory.Sequence(lambda n: 'Cohort %d' % (n,))
     description = factory.fuzzy.FuzzyText()
 
