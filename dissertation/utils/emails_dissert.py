992c4195cca741eb7d1f6b869a99b718e0052d50
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2017-03-28 15:16:17 +0200

    issue#1458 Refactor emails_dissert.py for codeclimate

--- dissertation/utils/emails_dissert.py
+++ dissertation/utils/emails_dissert.py
@@ -37,13 +37,10 @@ def get_base_template(dissert):
 
 
 def send_mail_to_teacher_new_dissert(dissert):
-
     html_template_ref = 'dissertation_adviser_new_project_dissertation_html'
     txt_template_ref = 'dissertation_adviser_new_project_dissertation_txt'
-    teacher_promoteur = get_promoteur_by_dissertation(dissert)
-    receivers = [message_config.create_receiver(teacher_promoteur.person.id,
-                                                teacher_promoteur.person.email,
-                                                teacher_promoteur.person.language)]
+    receivers = generate_receivers(get_promoteur_by_dissertation(dissert))
+
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
@@ -53,12 +50,10 @@ def send_mail_to_teacher_new_dissert(dissert):
 
 
 def send_mail_dissert_accepted_by_teacher(dissert):
-
     html_template_ref = 'dissertation_accepted_by_teacher_html'
     txt_template_ref = 'dissertation_accepted_by_teacher_txt'
-    receivers = [message_config.create_receiver(dissert.author.person.id,
-                                                dissert.author.person.email,
-                                                dissert.author.person.language)]
+    receivers = generate_receivers(dissert.author)
+
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
@@ -68,12 +63,10 @@ def send_mail_dissert_accepted_by_teacher(dissert):
 
 
 def send_mail_dissert_refused_by_teacher(dissert):
-
     html_template_ref = 'dissertation_refused_by_teacher_html'
     txt_template_ref = 'dissertation_refused_by_teacher_txt'
-    receivers = [message_config.create_receiver(dissert.author.person.id,
-                                                dissert.author.person.email,
-                                                dissert.author.person.language)]
+    receivers = generate_receivers(dissert.author)
+
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
@@ -83,12 +76,10 @@ def send_mail_dissert_refused_by_teacher(dissert):
 
 
 def send_mail_dissert_acknowledgement(dissert):
-
     html_template_ref = 'dissertation_acknowledgement_html'
     txt_template_ref = 'dissertation_acknowledgement_txt'
-    receivers = [message_config.create_receiver(dissert.author.person.id,
-                                                dissert.author.person.email,
-                                                dissert.author.person.language)]
+    receivers = generate_receivers(dissert.author)
+
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
@@ -98,13 +89,10 @@ def send_mail_dissert_acknowledgement(dissert):
 
 
 def send_mail_dissert_refused_by_com_to_student(dissert):
-
     html_template_ref = 'dissertation_refused_by_com_to_student_html'
     txt_template_ref = 'dissertation_refused_by_com_to_student_txt'
-    student_receiver = message_config.create_receiver(dissert.author.person.id,
-                                                dissert.author.person.email,
-                                                dissert.author.person.language)
-    receivers = [student_receiver]
+    receivers = generate_receivers(dissert.author)
+
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
@@ -114,15 +102,10 @@ def send_mail_dissert_refused_by_com_to_student(dissert):
 
 
 def send_mail_dissert_refused_by_com_to_teacher(dissert):
-
     html_template_ref = 'dissertation_refused_by_com_to_teacher_html'
     txt_template_ref = 'dissertation_refused_by_com_to_teacher_txt'
+    receivers = generate_receivers(get_promoteur_by_dissertation(dissert))
 
-    teacher_promoteur = get_promoteur_by_dissertation(dissert)
-    teachers_receiver = message_config.create_receiver(teacher_promoteur.person.id,
-                                                teacher_promoteur.person.email,
-                                                teacher_promoteur.person.language)
-    receivers = [teachers_receiver]
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
@@ -132,15 +115,22 @@ def send_mail_dissert_refused_by_com_to_teacher(dissert):
 
 
 def send_mail_dissert_accepted_by_com(dissert):
-
     html_template_ref = 'dissertation_accepted_by_com_html'
     txt_template_ref = 'dissertation_accepted_by_com_txt'
-    receivers = [message_config.create_receiver(dissert.author.person.id,
-                                              dissert.author.person.email,
-                                              dissert.author.person.language)]
+    receivers = generate_receivers(dissert.author)
+
     suject_data = None
     template_base_data = get_base_template(dissert)
     tables = None
     message_content = message_config.create_message_content(html_template_ref, txt_template_ref, tables, receivers,
                                                             template_base_data, suject_data)
     return message_service.send_messages(message_content)
+
+
+def generate_receivers(*receivers):
+    receivers_tab = []
+    for receiver in receivers:
+        receivers_tab.append(message_config.create_receiver(receiver.person.id,
+                                                            receiver.person.email,
+                                                            receiver.person.language))
+    return receivers_tab
