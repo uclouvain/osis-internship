0846923b09d9ec09a288c4317436cb6d5912bd52
DefatDavid <[defat.david]@gmail.com>
2017-03-30 11:32:57 +0200

    #1760 using travis cache

--- base/tests/functional/test_learning_units.py
+++ base/tests/functional/test_learning_units.py
@@ -35,7 +35,6 @@ import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 MAX_WAIT = 10
 
