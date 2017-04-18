7a4172ca5a51b163108ba4227c51574028bca705
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-04-18 10:20:58 +0200

    #1935

--- base/migrations/0103_auto_20170413_1034.py
+++ base/migrations/0103_auto_20170413_1034.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0101_employee_facultyadministrator'),
+        ('base', '0102_employee_facultyadministrator'),
     ]
 
     operations = [
