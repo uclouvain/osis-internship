8a19c5048569ec27118a7bcf773ce7e2f5797bf4
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2017-04-19 16:34:39 +0200

    #2082 Entity models creation (no migration file yet)

---
+++ base/models/enums/entity_type.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2017 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+SECTEUR = "SECTEUR"
+FACULTE = "FACULTE"
+ECOLE_OU_COMMISSION_D_ENSEIGNEMENT = "ECOLE_OU_COMMISSION_D_ENSEIGNEMENT"
+INSTITUT = "INSTITUT"
+POLE = "POLE"
+COMMISSION_DOCTORALE = "COMMISSION_DOCTORALE"
+PLATE_FORME = "PLATE_FORME"
+ENTITE_DE_LOGISTIQUE = "ENTITE_DE_LOGISTIQUE"
+NON_DEFINI = "NON_DEFINI"
+
+
+ENTITY_TYPES = (
+    (SECTEUR, SECTEUR),
+    (FACULTE, FACULTE),
+    (ECOLE_OU_COMMISSION_D_ENSEIGNEMENT, ECOLE_OU_COMMISSION_D_ENSEIGNEMENT),
+    (INSTITUT, INSTITUT),
+    (POLE, POLE),
+    (COMMISSION_DOCTORALE, COMMISSION_DOCTORALE),
+    (PLATE_FORME, PLATE_FORME),
+    (ENTITE_DE_LOGISTIQUE, ENTITE_DE_LOGISTIQUE),
+    (NON_DEFINI, NON_DEFINI),
+)
