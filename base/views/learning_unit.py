80334b120367d4e3e195defaad221519f6a46c20
Hildeberto Mendonca <me@hildeberto.com>
2017-05-04 15:10:50 +0200

    Redesigned the tabs in the learning unit interface.

--- base/views/learning_unit.py
+++ base/views/learning_unit.py
@@ -33,121 +33,77 @@ from attribution import models as mdl_attr
 from base import models as mdl
 from base.forms.learning_units import LearningUnitYearForm
 from . import layout
-from collections import OrderedDict
-
-
-TAB_IDENTIFICATION = 0
-TAB_TRAININGS = 1
-TAB_COMPONENTS = 2
-TAB_EDUCATIONAL_INFORMATION = 3
-TAB_ATTRIBUTIONS = 4
-TAB_PROPOSAL = 5
 
 
 @login_required
 @permission_required('base.can_access_learningunit', raise_exception=True)
 def learning_units(request):
-    template_name = "learning_units.html"
-    context = _get_common_context_list_learning_unit_years()
-    context.update({
-        'form': LearningUnitYearForm(),
-        'current_academic_year': mdl.academic_year.current_academic_year()
-    })
-    return layout.render(request, template_name, context)
-
-
-@login_required
-@permission_required('base.can_access_learningunit', raise_exception=True)
-def learning_units_search(request):
-    template_name = "learning_units.html"
-    form = LearningUnitYearForm(request.GET)
-    learning_units = None
+    if request.GET.get('academic_year'):
+        form = LearningUnitYearForm(request.GET)
+    else:
+        form = LearningUnitYearForm()
+    found_learning_units = None
     if form.is_valid():
-        learning_units = form.get_learning_units()
+        found_learning_units = form.get_learning_units()
         _check_if_display_message(request, learning_units)
 
     context = _get_common_context_list_learning_unit_years()
     context.update({
         'form': form,
         'academic_years': mdl.academic_year.find_academic_years(),
-        'learning_units': learning_units
+        'learning_units': found_learning_units,
+        'current_academic_year': mdl.academic_year.current_academic_year()
     })
-    return layout.render(request, template_name, context)
+    return layout.render(request, "learning_units.html", context)
 
 
 @login_required
 @permission_required('base.can_access_learningunit', raise_exception=True)
-def learning_unit_read(request, learning_unit_year_id):
+def learning_unit_identification(request, learning_unit_year_id):
     learning_unit_year = mdl.learning_unit_year.find_by_id(learning_unit_year_id)
-    attributions = mdl_attr.attribution.search(learning_unit_year=learning_unit_year)
-    enrollments = mdl.learning_unit_enrollment.find_by_learningunit_enrollment(learning_unit_year)
-    is_program_manager = mdl.program_manager.is_program_manager(request.user)
-
-    return layout.render(request, "learning_unit/identification.html", {
-        'learning_unit_year': learning_unit_year,
-        'tab_active': TAB_IDENTIFICATION,
-        'data_tab': get_tabs()})
+    tab_active = 'identification'
+    return layout.render(request, "learning_unit/identification.html", locals())
 
 
 @login_required
 @permission_required('base.can_access_learningunit', raise_exception=True)
-def detail(request, learning_unit_year_id, tab):
+def learning_unit_formations(request, learning_unit_year_id):
     learning_unit_year = mdl.learning_unit_year.find_by_id(learning_unit_year_id)
-    data = {'learning_unit_year': learning_unit_year,
-            'tab_active': tab,
-            'data_tab': get_tabs()}
-
-    mapping = {TAB_IDENTIFICATION: identification_detail,
-               TAB_TRAININGS:training_detail,
-               TAB_COMPONENTS: components_detail,
-               TAB_EDUCATIONAL_INFORMATION: educational_information_detail,
-               TAB_ATTRIBUTIONS: attributions_detail,
-               TAB_PROPOSAL: proposal_detail}
-
-    function = mapping.get(int(tab))
-    if function:
-        return function(request, data)
-
-    return layout.render(request, "learning_unit/identification.html", data)
+    tab_active = 'formations'
+    return layout.render(request, "learning_unit/formations.html", locals())
 
 
-def identification_detail(request, data):
-    return layout.render(request, "learning_unit/identification.html", data)
+@login_required
+@permission_required('base.can_access_learningunit', raise_exception=True)
+def learning_unit_components(request, learning_unit_year_id):
+    learning_unit_year = mdl.learning_unit_year.find_by_id(learning_unit_year_id)
+    tab_active = 'components'
+    return layout.render(request, "learning_unit/components.html", locals())
 
 
-def training_detail(request, data):
-    return layout.render(request, "learning_unit/trainings.html", data)
+@login_required
+@permission_required('base.can_access_learningunit', raise_exception=True)
+def learning_unit_pedagogy(request, learning_unit_year_id):
+    learning_unit_year = mdl.learning_unit_year.find_by_id(learning_unit_year_id)
+    tab_active = 'pedagogy'
+    return layout.render(request, "learning_unit/pedagogy.html", locals())
 
 
-def components_detail(request, data):
-    return layout.render(request, "learning_unit/components.html", data)
-
-
-def educational_information_detail(request, data):
-    return layout.render(request, "learning_unit/educational_information.html", data)
-
-
-def attributions_detail(request, data):
-    learning_unit_year = data['learning_unit_year']
+@login_required
+@permission_required('base.can_access_learningunit', raise_exception=True)
+def learning_unit_attributions(request, learning_unit_year_id):
+    learning_unit_year = mdl.learning_unit_year.find_by_id(learning_unit_year_id)
     attributions = mdl_attr.attribution.search(learning_unit_year=learning_unit_year)
-    data.update({'attributions': attributions})
-    return layout.render(request, "learning_unit/attributions.html", data)
+    tab_active = 'attributions'
+    return layout.render(request, "learning_unit/attributions.html", locals())
 
 
-def proposal_detail(request, data):
-    return layout.render(request, "learning_unit/proposal.html", data)
-
-
-def get_tabs():
-    #  Helps to construct tab pannel
-    tabs_order_and_label = {'0': _('identification'),
-                            '1': _('trainings'),
-                            '2': _('components'),
-                            '3': _('educational_information'),
-                            '4': _('tutor_attributions'),
-                            '5': _('proposal')}
-
-    return OrderedDict(sorted(tabs_order_and_label.items(), key=lambda t: t[0]))
+@login_required
+@permission_required('base.can_access_learningunit', raise_exception=True)
+def learning_unit_proposals(request, learning_unit_year_id):
+    learning_unit_year = mdl.learning_unit_year.find_by_id(learning_unit_year_id)
+    tab_active = 'proposals'
+    return layout.render(request, "learning_unit/proposals.html", locals())
 
 
 def _check_if_display_message(request, learning_units):
