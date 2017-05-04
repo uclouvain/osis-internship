48013fe57e9ea86b7c0b2bf7ba440eaccdbbe720
Mathieu Gerard <mathieu.gerard@uclouvain.be>
2017-05-04 09:21:50 +0200

    fix migration file

--- base/migrations/0111_entity_entityaddress_entitylink_entityversion.py
+++ base/migrations/0111_entity_entityaddress_entitylink_entityversion.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0109_sessionexam_offer_year'),
+        ('base', '0110_learningunityear_structure'),
     ]
 
     operations = [
