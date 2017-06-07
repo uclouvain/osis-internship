ef101a9b7e51f296f5a484536354ce17dd68a5f5
Bastien Mottiaux <bastien.mottiaux@uclouvain.be>
2017-06-07 15:59:59 +0200

    #2425 updated code quality

--- assessments/utils/remove_offers_2M_2MS.py
+++ assessments/utils/remove_offers_2M_2MS.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 def _set_parent_foreign_key_to_null():
     from base.models.offer_year import OfferYear
     for off in list(OfferYear.objects.all()):
@@ -9,14 +10,13 @@ def _remove_2M_2MS_offers():
     from django.db.models import Q
     from base.models.offer_year import OfferYear
     cpt = 0
-    for off in list(OfferYear.objects.filter(Q(acronym__endswith='2MS') | Q(acronym__endswith='2M'))\
+    for cpt, off in enumerate(OfferYear.objects.filter(Q(acronym__endswith='2MS') | Q(acronym__endswith='2M'))\
                                              .exclude(acronym__icontains='/')\
                                              .order_by('academic_year__year')
                                              .select_related('academic_year')):
         off.delete()
         print('{} ({}) was removed'.format(off.acronym, off.academic_year))
-        cpt += 1
-    print("{} OfferYear records were deleted.".format(cpt))
+    print("{} OfferYear records were deleted.".format(cpt+1))
 
 
 def execute():
@@ -24,4 +24,5 @@ def execute():
     _remove_2M_2MS_offers()
 
 
-execute()
+if __name__ == '__main__':
+    execute()
