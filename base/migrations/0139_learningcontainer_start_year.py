c1648f8d7dee020ae0a08eabf9b47557ae229b14
Hildeberto Mendonca <me@hildeberto.com>
2017-06-27 09:45:07 +0200

    Fixed migration file.

--- base/migrations/0139_learningcontainer_start_year.py
+++ base/migrations/0139_learningcontainer_start_year.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0137_auto_20170620_1042'),
+        ('base', '0138_auto_20170626_0938'),
     ]
 
     operations = [
