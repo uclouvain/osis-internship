ddee0eaa80c6c4bd79e89c0eed083df12659d44b
Laurent Buset <laurent.buset@uclouvain.be>
2017-07-10 16:31:40 +0200

    add button to filter form

--- assistant/forms.py
+++ assistant/forms.py
@@ -109,8 +109,7 @@ class AssistantFormPart1(ModelForm):
 
 
 class MandatesArchivesForm(ModelForm):
-    academic_year = forms.ModelChoiceField(queryset=academic_year.AcademicYear.objects.all(),
-                                           widget=forms.Select(attrs={"onChange": 'submit()'}))
+    academic_year = forms.ModelChoiceField(queryset=academic_year.AcademicYear.objects.all())
 
     class Meta:
         model = mdl.assistant_mandate.AssistantMandate
