c26b9a6c49ae997dc667e36bc5e06d280fda9ef7
verpoorten <leila.verpoorten@uclouvain.be>
2017-07-19 14:51:35 +0200

    Remove print

--- base/views/student.py
+++ base/views/student.py
@@ -53,8 +53,7 @@ def student_search(request):
 
 @login_required
 @user_passes_test(mdl.program_manager.is_program_manager)
-def student_read(request, registration_id):
-    print('student_read')
+def student_read(request, registration_id):    
     student = mdl.student.find_by_id(registration_id)
     if student:
         offers_enrollments = mdl.offer_enrollment.find_by_student(student)
