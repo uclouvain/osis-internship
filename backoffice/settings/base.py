10256d33055846e0f1960ec771df03a124aad96f
Christophe Fivet <christophe.fivet@uclouvain.be>
2017-07-19 08:58:55 +0200

    #1468 Modification du nom de la DB et de son utilisateur

--- backoffice/settings/base.py
+++ backoffice/settings/base.py
@@ -118,8 +118,8 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DATABASE_NAME", 'osis_backend_dev'),
-        'USER': os.environ.get("POSTGRES_USER", 'osis_usr'),
+        'NAME': os.environ.get("DATABASE_NAME", 'osis_local'),
+        'USER': os.environ.get("POSTGRES_USER", 'osis'),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'osis'),
         'HOST': os.environ.get("POSTGRES_HOST", '127.0.0.1'),
         'PORT': os.environ.get("POSTGRES_PORT", '5432'),
