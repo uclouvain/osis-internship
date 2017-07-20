5c9c0e73de49471b8b0b8e94d825ade77a311b6c
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-07-20 10:27:37 +0200

    #2583

--- base/models/education_group_year.py
+++ base/models/education_group_year.py
@@ -24,57 +24,24 @@
 #
 ##############################################################################
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from base.models import offer, program_manager, academic_year
 from osis_common.models.serializable_model import SerializableModel, SerializableModelAdmin
 
 
 class EducationGroupYearAdmin(SerializableModelAdmin):
     list_display = ('acronym', 'title', 'academic_year', 'education_group_type', 'changed')
-    fieldsets = ((None, {'fields': ('academic_year', 'entity_administration', 'entity_administration_fac',
-                                    'entity_management', 'entity_management_fac', 'acronym', 'title',
-                                    'title_international', 'title_short', 'title_printable', 'grade', 'grade_type',
-                                    'recipient', 'location', 'postal_code', 'city', 'country', 'phone', 'fax', 'email',
-                                    'campus', 'education_group_type', 'education_group')}),)
-    list_filter = ('academic_year', 'grade', 'education_group_type', 'campus')
-    raw_id_fields = ('education_group_type', 'grade_type','campus','country','entity_administration',
-                     'entity_administration_fac', 'entity_management', 'entity_management_fac', 'academic_year',
-                     'education_group')
+    fieldsets = ((None, {'fields': ('academic_year', 'acronym', 'title', 'education_group_type', 'education_group')}),)
+    list_filter = ('academic_year', 'education_group_type')
+    raw_id_fields = ('education_group_type', 'academic_year', 'education_group')
     search_fields = ['acronym']
-
-
-GRADE_TYPES = (
-    ('BACHELOR', _('bachelor')),
-    ('MASTER', _('master')),
-    ('DOCTORATE', _('ph_d')))
 
 
 class EducationGroupYear(SerializableModel):
     external_id = models.CharField(max_length=100, blank=True, null=True)
     changed = models.DateTimeField(null=True, auto_now=True)
-    education_group = models.ForeignKey('EducationGroup')
-    academic_year = models.ForeignKey('AcademicYear')
     acronym = models.CharField(max_length=15, db_index=True)
     title = models.CharField(max_length=255)
-    title_international = models.CharField(max_length=255, blank=True, null=True)
-    title_short = models.CharField(max_length=255, blank=True, null=True)
-    title_printable = models.CharField(max_length=255, blank=True, null=True)
-    grade = models.CharField(max_length=20, blank=True, null=True, choices=GRADE_TYPES)
-    entity_administration = models.ForeignKey('Structure', related_name='education_group_admministration', blank=True, null=True)
-    entity_administration_fac = models.ForeignKey('Structure', related_name='education_group_admministration_fac', blank=True, null=True)
-    entity_management = models.ForeignKey('Structure', related_name='education_group_management', blank=True, null=True)
-    entity_management_fac = models.ForeignKey('Structure', related_name='education_group_management_fac', blank=True, null=True)
-    recipient = models.CharField(max_length=255, blank=True, null=True)  # Recipient of scores cheets (Structure)
-    location = models.CharField(max_length=255, blank=True, null=True)  # Address for scores cheets
-    postal_code = models.CharField(max_length=20, blank=True, null=True)
-    city = models.CharField(max_length=255, blank=True, null=True)
-    country = models.ForeignKey('reference.Country', blank=True, null=True)
-    phone = models.CharField(max_length=30, blank=True, null=True)
-    fax = models.CharField(max_length=30, blank=True, null=True)
-    email = models.EmailField(null=True, blank=True)
-    campus = models.ForeignKey('Campus', blank=True, null=True)
-    grade_type = models.ForeignKey('reference.GradeType', blank=True, null=True)
-    enrollment_enabled = models.BooleanField(default=False)
+    academic_year = models.ForeignKey('AcademicYear')
+    education_group = models.ForeignKey('EducationGroup')
     education_group_type = models.ForeignKey('OfferType', blank=True, null=True)
 
     def __str__(self):
