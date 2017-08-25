25cac302cabab9a9e6f9999ea9f2b7edc0b9411a
Christophe Fivet <christophe.fivet@uclouvain.be>
2017-08-25 13:50:54 +0200

    #2719

--- base/views/learning_unit.py
+++ base/views/learning_unit.py
@@ -556,7 +556,8 @@ def learning_class_year_edit(request, learning_unit_year_id):
 def learning_unit_create(request, academic_year):
     form = CreateLearningUnitYearForm(initial={'academic_year': academic_year,
                                                'subtype': "FULL",
-                                               'learning_container_year_type': "---------"})
+                                               'learning_container_year_type': "---------",
+                                               'language': 3})
     return layout.render(request, "learning_unit/learning_unit_form.html", {'form': form})
 
 
