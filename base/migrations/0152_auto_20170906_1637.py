adb2bcf01239928e06245ac9f36275e8f6692917
Eddy Ndizera <eddyndizera@gmail.com>
2017-09-06 17:01:12 +0200

    removed learning_unit_year migration

--- base/migrations/0152_auto_20170906_1637.py
+++ base/migrations/0152_auto_20170906_1637.py
@@ -17,9 +17,4 @@ class Migration(migrations.Migration):
             name='highlight_title',
             field=models.CharField(blank=True, max_length=50, null=True),
         ),
-        migrations.AlterField(
-            model_name='learningunityear',
-            name='session',
-            field=models.CharField(blank=True, choices=[('1', '1'), ('2', '2'), ('3', '3'), ('12', '12'), ('13', '13'), ('23', '23'), ('123', '123'), ('P23', 'P23')], max_length=50, null=True),
-        ),
     ]
