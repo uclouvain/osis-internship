786727f8f2792c6ec86b2192f2ef33901c321c60
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-09-22 11:01:18 +0200

    Correcrion dependence

--- base/migrations/0157_learning_unit_component_clean.py
+++ base/migrations/0157_learning_unit_component_clean.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '0157_learning_unit_component_clean'),
+        ('base', '0156_offeryearentity_education_group_year'),
     ]
 
     operations = [
