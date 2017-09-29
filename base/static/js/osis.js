413f8b91a89fc3e1777838988794826c66e9bd6a
Alessandro Brugnetti <alessandro.brugnetti@uclouvain.be>
2017-09-29 17:44:58 +0200

    #OSIS-36 Store in localstorage and implemente template_nav_header in order to have generic behaviour

--- base/static/js/osis.js
+++ base/static/js/osis.js
@@ -139,3 +139,24 @@ var browser_supported_versions = {
     safari: 8,
     edge: 24
 }
+
+function setEventKeepIds(domTableId, localStorageKey) {
+    //Check if localstorage is supported
+    if(typeof localStorage!='undefined') {
+        var table = $('#' + domTableId)
+        if (table != undefined) {
+            table.on('order.dt', function() {
+                var orderList = [];
+                table.find('tr').each(function() {
+                    var id = $(this).attr('data-id');
+                    if (id != undefined) {
+                         orderList.push(id);
+                    }
+                });
+                if(orderList.length > 0){
+                    localStorage.setItem(localStorageKey, JSON.stringify(orderList));
+                }
+            });
+        }
+    }
+}
