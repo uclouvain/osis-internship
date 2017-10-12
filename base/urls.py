aabae3234937826bbbfdca57e14cce5b39c0c303
Leïla Verpoorten <leila.verpoorten@uclouvain.be>
2017-10-12 15:17:17 +0200

    Modification urls

--- base/urls.py
+++ base/urls.py
@@ -179,4 +179,4 @@ urlpatterns = [
             url(r'^picture$', student.student_picture, name='student_picture'),
         ]))
     ])),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
