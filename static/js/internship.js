4b58ffc3bec3a7f8b8e8df465c14242678a2c8b2
Mathieu Zen <zen.mathieu@gmail.com>
2018-04-20 15:16:40 +0200

    add title with element name in delete confirmation modal

--- static/js/internship.js
+++ static/js/internship.js
@@ -1,3 +1,4 @@
   $('#delete_modal').on('show.bs.modal', (e) => {
       $('#btn-ok').attr('href', $(e.relatedTarget).data('href'));
+      $('#delete-element').html($(e.relatedTarget).data('value'));
   });
