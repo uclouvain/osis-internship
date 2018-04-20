50b930448dbca5856284d528ed4fdae96481552b
Mathieu Zen <zen.mathieu@gmail.com>
2018-04-20 16:17:01 +0200

    sumup tabs converted in dropdown list

--- static/js/internship.js
+++ static/js/internship.js
@@ -1,4 +1,33 @@
+  /* OSIS stands for Open Student Information System. It's an application
+* designed to manage the core business of higher education institutions,
+* such as universities, faculties, institutes and professional schools.
+* The core business involves the administration of students, teachers,
+* courses, programs and so on.
+*
+* Copyright (C) 2017-2018 Université catholique de Louvain (http://www.uclouvain.be)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* A copy of this license - GNU General Public License - is available
+* at the root of the source code of this program.  If not,
+* see http://www.gnu.org/licenses/.
+*
+* OSIS Custom layout
+*/
+
   $('#delete_modal').on('show.bs.modal', (e) => {
       $('#btn-ok').attr('href', $(e.relatedTarget).data('href'));
       $('#delete-element').html($(e.relatedTarget).data('value'));
+  });
+
+  $(".dropdown-menu li a").click(function(){
+    $(".btn:first-child").html($(this).text()+' <span class="caret"></span>');
   });
