cdf05092e100dc31a21afb08bfbe97bd3dc7066c
Mathieu Zen <zen.mathieu@gmail.com>
2018-05-11 14:40:54 +0200

    test valid master form

---
+++ tests/forms/test_master_form.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2017 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.test import TestCase
+
+from internship.forms import master
+from internship.tests.factories.cohort import CohortFactory
+from base.tests.factories.person import PersonFactory
+
+
+class TestMasterForm(TestCase):
+
+    def test_valid_form(self):
+        data = {
+            "first_name": "test",
+            "last_name": "test",
+            "civility": "DOCTOR",
+            "gender": "M",
+            "email": "test@test.com",
+            "email_private": "test@test.com",
+            "phone_mobile": "046486313",
+            "location": "location",
+            "postal_code": "1348",
+            "city": "city",
+            "country": "country",
+            'birth_date': "1980-01-01",
+            'start_activities': "1980-01-01",
+        }
+        form = master.MasterForm(data)
+        self.assertTrue(form.is_valid())
+
