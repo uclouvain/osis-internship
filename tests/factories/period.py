a2e00c30fe3b3755bc4cf43cb228fa10c4c53cc3
Mathieu Zen <zen.mathieu@gmail.com>
2019-03-27 17:06:05 +0100

    INTERNSHIP-49 generate period name starting with 'P'

--- tests/factories/period.py
+++ tests/factories/period.py
@@ -33,7 +33,7 @@ class PeriodFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'internship.Period'
 
-    name = factory.Sequence(lambda n: str(n))
+    name = factory.Sequence(lambda n: 'P%d' % n)
 
     date_start = factory.LazyAttribute(lambda obj: pendulum.today().start_of('month')._datetime)
     date_end = factory.LazyAttribute(lambda obj: pendulum.instance(obj.date_start).end_of('month')._datetime)
