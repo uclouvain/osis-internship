6826448821186aa50dd1583f95a6c15b5b536fdd
Mathieu Zen <zen.mathieu@gmail.com>
2019-05-14 10:47:42 +0200

    INTERNSHIP-100 fix test

--- tests/views/test_scores.py
+++ tests/views/test_scores.py
@@ -32,6 +32,7 @@ from django.utils.translation import gettext as _
 from rest_framework import status
 
 from base.tests.factories.student import StudentFactory
+from internship.models.internship_score import InternshipScore
 from internship.tests.factories.cohort import CohortFactory
 from internship.tests.factories.internship_student_information import InternshipStudentInformationFactory
 from internship.tests.factories.period import PeriodFactory
@@ -59,12 +60,13 @@ class ScoresEncodingTest(TestCase):
         self.students = [InternshipStudentInformationFactory(cohort=self.cohort) for _ in range(11)]
         for student_info in self.students:
             student = StudentFactory(person=student_info.person)
-            ScoreFactory(student=student, period=self.period, cohort=self.cohort)
+            ScoreFactory(student=student, period=self.period, cohort=self.cohort, APD_1='A')
         for apd in range(1, 15):
-            self.mapping = ScoreMappingFactory(
+            ScoreMappingFactory(
                 period=self.period,
                 cohort=self.cohort,
-                score_A=1, score_B=1, score_C=1, score_D=1
+                score_A=1, score_B=1, score_C=1, score_D=1,
+                apd=apd
             )
 
     def test_view_scores_encoding(self):
@@ -116,7 +118,7 @@ class ScoresEncodingTest(TestCase):
         response = self.client.get(url)
         student_scores = response.context['students'].object_list[0].scores
         self.assertEqual(student_scores[0][0], self.period.name)
-        self.assertTrue(student_scores[0][1][0] in ['A', 'B', 'C', 'D', 'E'])
+        self.assertIn(student_scores[0][1][0], [score[0] for score in InternshipScore.SCORE_CHOICES])
 
     def test_search_student_by_name(self):
         url = reverse('internship_scores_encoding', kwargs={'cohort_id': self.cohort.pk})
