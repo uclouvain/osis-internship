22cfbe55aee5337530b6de7bb41435214c5cdcac
Mathieu Zen <zen.mathieu@gmail.com>
2019-05-16 10:46:32 +0200

    INTERNSHIP-104 retrieve existing mapping values saved in db

---
+++ templatetags/mapping.py
@@ -0,0 +1,38 @@
+##############################################################################
+#
+#    OSIS stands for Open Student Information System. It's an application
+#    designed to manage the core business of higher education institutions,
+#    such as universities, faculties, institutes and professional schools.
+#    The core business involves the administration of students, teachers,
+#    courses, programs and so on.
+#
+#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of this license - GNU General Public License - is available
+#    at the root of the source code of this program.  If not,
+#    see http://www.gnu.org/licenses/.
+#
+##############################################################################
+from django.template.defaulttags import register
+
+
+@register.simple_tag
+def get_mapping_value(mapping, period, apd, score):
+    try:
+        return vars(list(filter(_get_mapping(period, apd), mapping))[0])['score_{}'.format(score)]
+    except IndexError:
+        return None
+
+
+def _get_mapping(period, apd):
+    return lambda x: period.name == x.period.name and x.apd == apd
