9bd10d23d681992bcf28e7ffedeff9d1930e6f33
Mathieu Zen <zen.mathieu@gmail.com>
2019-06-19 12:16:23 +0200

    INTERNSHIP-88 covering offer page with choices

--- tests/views/test_offer.py
+++ tests/views/test_offer.py
@@ -44,16 +44,33 @@ class OfferViewTestCase(TestCase):
         permission = Permission.objects.get(codename='is_internship_manager')
         self.user.user_permissions.add(permission)
         self.client.force_login(self.user)
-
         self.cohort = CohortFactory()
+        self.organization = OrganizationFactory(cohort=self.cohort)
+        self.specialty = SpecialtyFactory(mandatory=True, cohort=self.cohort)
+        self.offer = OfferFactory(cohort=self.cohort, organization=self.organization,  speciality=self.specialty)
+        students = [StudentFactory() for _ in range(0, 4)]
+        mandatory_internship = InternshipFactory(cohort=self.cohort, speciality=self.specialty)
+        non_mandatory_internship = InternshipFactory(cohort=self.cohort)
+
+        for internship in [mandatory_internship, non_mandatory_internship]:
+            for choice in [1, 2, 3, 4]:
+                create_internship_choice(
+                    organization=self.organization,
+                    student=students[0],
+                    speciality=self.specialty,
+                    choice=choice,
+                    internship=internship
+                )
+        for student in students[1:]:
+            create_internship_choice(
+                organization=self.organization,
+                student=student,
+                speciality=self.specialty,
+                choice=random.choice([2, 3, 4]),
+                internship=mandatory_internship
+            )
 
     def test_home(self):
-        # FIXME: specify organization_sort and speciality_sort
-        speciality = SpecialtyFactory(
-            mandatory=True,
-            cohort=self.cohort
-        )
-
         url = reverse('internships', kwargs={
             'cohort_id': self.cohort.id,
         })
@@ -61,29 +78,47 @@ class OfferViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'internships.html')
 
-    def test_home_with_offer(self):
+    def test_home_with_organization_filter(self):
         organization = OrganizationFactory(cohort=self.cohort)
-        speciality = SpecialtyFactory(
-            mandatory=True,
-            cohort=self.cohort
-        )
-        offer = OfferFactory(organization=organization, speciality=speciality)
-
         url = reverse('internships', kwargs={
             'cohort_id': self.cohort.id,
-            'specialty_id': speciality.id
         })
+        get_params = '?organization_sort={}'.format(organization.name)
+        response = self.client.get(url+get_params)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'internships.html')
 
+    def test_home_with_specialty_filter(self):
+        url = reverse('internships', kwargs={
+            'cohort_id': self.cohort.id,
+        })
+        get_params = '?speciality_sort={}'.format(self.specialty.name)
+        response = self.client.get(url+get_params)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'internships.html')
+
+    def test_home_with_both_filters(self):
+        url = reverse('internships', kwargs={
+            'cohort_id': self.cohort.id,
+        })
+        get_params = '?speciality_sort={}&organization_sort={}'.format(self.specialty.name, self.organization.name)
+        response = self.client.get(url+get_params)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'internships.html')
+
+    def test_home_with_offer(self):
+        url = reverse('internships', kwargs={
+            'cohort_id': self.cohort.id,
+            'specialty_id': self.specialty.id
+        })
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'internships.html')
 
     def test_internship_detail_student_choice(self):
-        offer = OfferFactory(organization=OrganizationFactory(cohort=self.cohort))
-
         url = reverse('internship_detail_student_choice', kwargs={
             'cohort_id': self.cohort.id,
-            'offer_id': offer.id,
+            'offer_id': self.offer.id,
         })
 
         response = self.client.get(url)
@@ -91,7 +126,7 @@ class OfferViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'internship_detail.html')
 
-        self.assertEqual(response.context['internship'], offer)
+        self.assertEqual(response.context['internship'], self.offer)
 
 
 class OfferChoiceDistributionTestCase(TestCase):
