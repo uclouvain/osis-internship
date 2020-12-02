75fe753ce923a8f1f41a98e29481ac6571f4c406
Mathieu Zen <zen.mathieu@gmail.com>
2020-12-02 10:32:33 +0100

    INTERNSHIP-208 fix copyright + file name

--- forms/internship_person_form.py
+++ forms/internship_person_form.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2019 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2020 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -29,6 +29,11 @@ from base.models.person import Person
 
 
 class InternshipPersonForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(InternshipPersonForm, self).__init__(*args, **kwargs)
+        self.fields['last_name'].required = True
+        self.fields['first_name'].required = True
 
     class Meta:
         model = Person
