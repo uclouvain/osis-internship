bd48eabf2ce5b3443d4cb19ce34d4b1e80ad17cb
Mathieu Zen <zen.mathieu@gmail.com>
2020-12-09 15:38:50 +0100

    INTERNSHIP-208 check exist before gest

--- migrations/0093_internshipmaster_person.py
+++ migrations/0093_internshipmaster_person.py
@@ -15,7 +15,7 @@ def create_persons(apps, schema_editor):
 
     for master in InternshipMaster.objects.all():
         # check master has already a person in db that should be linked to
-        if "@uclouvain.be" in master.email:
+        if "@uclouvain.be" in master.email and Person.objects.filter(email=master.email).exists():
             existing_person = Person.objects.get(email=master.email)
             for field, value in {field: getattr(master, field) for field in common_fields}.items():
                 if not getattr(existing_person, field):
