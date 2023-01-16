488481c6f708a3bad7e4233a73d7e4e01198ef27
mathieuzen <zen.mathieu@gmail.com>
2023-01-16 11:00:05 +0100

    Revert "Merge pull request #329 from uclouvain/rollback-qa"
    This reverts commit 548791ea097fab7c082bb731324a120cf801f03a, reversing
    changes made to 7234c033482d4bf846ffb9226eb3597acbae5add.

--- api/serializers/internship_place_evaluation.py
+++ api/serializers/internship_place_evaluation.py
@@ -6,7 +6,7 @@
 #    The core business involves the administration of students, teachers,
 #    courses, programs and so on.
 #
-#    Copyright (C) 2015-2021 Université catholique de Louvain (http://www.uclouvain.be)
+#    Copyright (C) 2015-2022 Université catholique de Louvain (http://www.uclouvain.be)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,16 +23,14 @@
 #    see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-from django import forms
-from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
 
-from internship.models.internship_place_evaluation_item import PlaceEvaluationItem
+from internship.models.internship_place_evaluation import PlaceEvaluation
 
 
-class PlaceEvaluationItemForm(forms.ModelForm):
-    statement = forms.CharField(label=_('Statement'), widget=forms.Textarea(attrs={'rows': 5}))
-    options = forms.CharField(label=_('Options'), required=False)
-
+class InternshipStudentPlaceEvaluationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PlaceEvaluationItem
-        fields = ['statement', 'type', 'options', 'active', 'required']
+        model = PlaceEvaluation
+        fields = (
+            'evaluation',
+        )
--- tests/factories/period.py
+++ tests/factories/period.py
@@ -40,3 +40,4 @@ class PeriodFactory(factory.django.DjangoModelFactory):
     cohort = factory.SubFactory(CohortFactory)
     reminder_mail_sent = False
     remedial = False
+    place_evaluation_active = False
